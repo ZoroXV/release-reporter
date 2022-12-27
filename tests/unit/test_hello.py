e5910adbbe7e7d564417061a78ddcf839a8fc299
Victor LE CORRE <victor.le-corre@epita.fr>
2022-12-27 18:18:07 +0100

    feat: add python basic project files

---
+++ tests/unit/test_hello.py
@@ -0,0 +1,10 @@
+import pytest
+import releasereporter as rr
+
+class TestEvent():
+    # def setup_class(self):
+
+    # def teardown_method(self):
+
+    def test_hello(self):
+        assert True
