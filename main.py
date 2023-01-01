d200e280de72fe924f0ed1b037c412532f7551da
Victor LE CORRE <victor.le-corre@epita.fr>
2023-01-01 19:49:01 +0100

    feat: Add basic bot
    Only respond to the command "!hello" with the Discord User Nickname

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-import releasereporter as rr
+# import releasereporter as rr
 
-def main():
-    rr.hello()
+# def main():
+#     rr.hello()
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+import os
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+TOKEN = os.environ.get('DISCORD_TOKEN')
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Logged in {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.channel.send(f'Hello, {message.author.nick}!')
+
+client.run(TOKEN)
